f5af7a6ca926ba477fbdc34425073390975eea5e
Ayoub Jdad <ayoub.jdad@weexa.com>
2025-09-30 12:57:52 +0100

    feat: add TextCenter component with styling and integrate into Home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,13 @@
 import Header from "../../layouts/header/Header";
 import Main from "../../layouts/main/Main";
+import TextCenter from "../../layouts/text-center/TextCenter";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <Main />
+      <TextCenter />
     </div>
   );
 }
